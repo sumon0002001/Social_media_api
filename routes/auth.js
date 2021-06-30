2bc1e07e820ff33d2f8301c599914aa24f04e538
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-07-01 03:05:48 +0600

    Add database

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,25 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/",(req, res) => {
-  res.send("Hey its from auth router")
-})
+
+//Register
+router.post("/register", async(req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.username,
+      });
+
+      try {
+          const user = await newUser.save();
+          res.status(200).json(user);
+      } catch(error){
+          console.log(error);
+      }
+  
+
+    }
+)
 
 
 module.exports = router
