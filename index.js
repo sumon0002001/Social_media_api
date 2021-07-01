238344a7f2d4d1396dc2b98b123f49e2b3341aa8
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-07-01 22:28:18 +0600

    Add functions to bring all posts and individual post

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
 
 dotenv.config();
 
@@ -23,6 +25,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 
 
